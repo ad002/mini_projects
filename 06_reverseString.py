8e3912dc4009eb0095820c9281747706bbfe7edc
ad002 <adrianhunold@gmx.de>
2019-03-19 08:00:31 +0100

    New lesson. Need some practice

---
+++ 06_reverseString.py
@@ -0,0 +1,29 @@
+#Take user input and split into single letters (split returns a list) 
+x=input("Gimme some text to check whether palindrome \n > ")
+
+#https://www.geeksforgeeks.org/reverse-string-python-5-different-ways/
+
+#Solution 1: Using loops 
+#def reverse_1(x):
+    #Taking string as input 
+#    str=''
+    #until we haven't reached the end of the string
+#    for i in x:
+        #put in every ith element of x in str
+#        str=i+str
+#    return str
+    
+
+#print(revString(x))
+
+
+#x = x[::-1] 
+
+#Change order / Reverse string 
+#for i in range(len(x)):
+    #first item = last item (-1 because of 0 index)-counter variable
+#    x[i] = x[(len(x-1))-i]	
+    
+#print(x)
+
+	
